9c03255d5ab2cd7795d1ab1f16d5c3c864d91b64
ValeriyaHr <152651276+ValeriyaHr@users.noreply.github.com>
2024-04-27 16:56:40 +0300

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,39 +1,25 @@
-// Об'єкт для зберігання даних форми
-let formData = { email: "", message: "" };
-
-const form = document.querySelector('.feedback-form');
-const emailInput = form.querySelector('input[name="email"]');
-const messageTextarea = form.querySelector('textarea[name="message"]');
-
-function updateLocalStorage() {
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+const formData = {
+ email: "",
+ message: "",
 }
 
-form.addEventListener('input', (event) => {
-  const { name, value } = event.target;
-  formData[name] = value.trim(); // trim() видаляє пробіли з кінців рядка
-  updateLocalStorage();
-});
-
-const savedData = localStorage.getItem('feedback-form-state');
-if (savedData) {
-  const savedFormData = JSON.parse(savedData);
-  emailInput.value = savedFormData.email || '';
-  messageTextarea.value = savedFormData.message || '';
-  formData.email = savedFormData.email || '';
-  formData.message = savedFormData.message || '';
-}
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault(); 
-  if (formData.email === "" || formData.message === "") {
-    alert("Fill please all fields");
-    return;
-  }
-
-  console.log(formData); 
-  localStorage.removeItem('feedback-form-state'); 
-  formData.email = "";
-  formData.message = "";
-  form.reset(); 
-});
+const form = document.querySelector(".feedback-form");
+const formStringData = localStorage.getItem("feedback-form-state") ?? "";
+const formParseData = formStringData ? JSON.parse(formStringData) : { email: "", message: "" };
+form.elements.email.value = formParseData.email;
+form.elements.message.value = formParseData.message;
+form.addEventListener("input", () => {
+    formData.email = form.elements.email.value.trim()
+    formData.message = form.elements.message.value.trim()
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+    
+})
+form.addEventListener("submit", evt => {
+    evt.preventDefault()
+    if (formData.email === "" || formData.message === "") {
+        return alert("All form fields must be filled in")
+    }
+    console.log(formData)
+    localStorage.removeItem("feedback-form-state")
+    form.reset()
+})
