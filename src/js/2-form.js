4902ac385e7362cb6346a696a6e6293b627ae3c5
ValeriyaHr <152651276+ValeriyaHr@users.noreply.github.com>
2024-04-28 16:59:44 +0300

    1

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,25 +1,40 @@
 const formData = {
- email: "",
- message: "",
-}
-
-const form = document.querySelector(".feedback-form");
-const formStringData = localStorage.getItem("feedback-form-state") ?? "";
-const formParseData = formStringData ? JSON.parse(formStringData) : { email: "", message: "" };
-form.elements.email.value = formParseData.email;
-form.elements.message.value = formParseData.message;
-form.addEventListener("input", () => {
-    formData.email = form.elements.email.value.trim()
-    formData.message = form.elements.message.value.trim()
-    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
-    
-})
-form.addEventListener("submit", evt => {
-    evt.preventDefault()
+    email: "",
+    message: "",
+  };
+  
+  const form = document.querySelector(".feedback-form");
+  
+  const savedData = localStorage.getItem("feedback-form-state");
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    formData.email = parsedData.email;
+    formData.message = parsedData.message;
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+  
+  form.addEventListener("input", () => {
+    formData.email = form.elements.email.value.trim();
+    formData.message = form.elements.message.value.trim();
+  
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+  });
+  
+  form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+  
     if (formData.email === "" || formData.message === "") {
-        return alert("All form fields must be filled in")
+      alert("Fill please all fields");
+      return;
     }
-    console.log(formData)
-    localStorage.removeItem("feedback-form-state")
-    form.reset()
-}) 
+  
+    console.log("Form Data:", formData);
+  
+    localStorage.removeItem("feedback-form-state");
+    formData.email = "";
+    formData.message = "";
+  
+    form.reset();
+  });
+  
